192dc6a62a7b62e694289793c49e37fc875a770a
AndreiLegram <andrei.legramante@gmail.com>
2024-09-03 20:09:40 -0300

    aula 030924

--- react-my-form/src/main.jsx
+++ react-my-form/src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import ComHookStateEffect from './componentes/ComHookStateEffect';
+import App from './App'
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ComHookStateEffect/>
+    <App />
   </React.StrictMode>,
 )
