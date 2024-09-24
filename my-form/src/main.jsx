2de46f628764998ca87565079f30fec6eb1d9c3f
AndreiLegram <andrei.legramante@gmail.com>
2024-09-24 20:15:47 -0300

    aula 2409

---
+++ my-form/src/main.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import FormInput from './componentes/FormInput'
+import FormMultipleInputs from './componentes/FormMultipleInputs'
+import FormTextArea from './componentes/FormTextArea'
+import FormSelect from './componentes/FormSelect'
+import FormValidation from './componentes/FormValidation'
+import FormErrorMessage from './componentes/FormErrorMessage'
+import NameFormNC from './componentes/NameFormNC'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <FormInput /><br/>
+    <FormMultipleInputs /><br/>
+    <FormTextArea /><br/>
+    <FormSelect /><br/>
+    <FormValidation /><br/>
+    <FormErrorMessage /><br/>
+    <NameFormNC /><br/>
+  </React.StrictMode>,
+)
